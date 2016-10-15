8ebd07b842661fd599300465f7408420cb166d04
Diana V <drv687@users.noreply.github.com>
2016-10-15 19:41:27 -0400

    Add files via upload

---
+++ test.js
@@ -0,0 +1,5 @@
+var userName;  
+//Prompt the user for their name
+userName = prompt("Please enter your name.");
+document.write("Hello" + "" +userName); //Say Hello to the user
+confirm("You entered" + "" +userName);//Confirm the user's name. 
